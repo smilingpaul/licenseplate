521a7e6f4be3fa9fd9420f1a4485dfa4d46f48ee
esbenbugge <esbenbugge@1f401a1f-e745-0410-a33a-219f3c91b65c>
2008-03-04 21:14:31 +0000

    plate_rotate.m: test of hough started

--- trunk/src/detection/plate_rotate.m
+++ trunk/src/detection/plate_rotate.m
@@ -15,13 +15,16 @@ function [rotated_img] = plate_rotate (img)
   %figure, imshow(bw);
 
   % compute radon transform of edge image
-  
   [R,xp] = radon(bw);
+  
   %size(R)
   %radon_matrix = (theta, xp, R);
   %figure, imshow(R); colormap(hot);
-  %imwrite(imagesc(0:179, xp, R),'P_Radon.jpg');
-  figure, imagesc(0:179, xp, R); colormap(hot);
+  %imwrite(imagesc(theta, xp, R),'P_Radon.jpg');
+
+  % display radon matrix
+  theta = 0:179;
+  figure, imagesc(theta, xp, R); colormap(hot);
   xlabel('\theta (grader)'); ylabel('x\prime');
   title('R_{\theta} {x\prime}');
   colorbar
@@ -29,8 +32,20 @@ function [rotated_img] = plate_rotate (img)
   % find degree of which the largest registration in Radon transformation matrix was found
   [x,degree] = max(max(abs(R)))
 
+  % TEST OF HOUGH
+  [H,T,rho] = hough(bw);
+
+  %display hough matrix
+  figure, subplot(2,1,2);
+  imshow(imadjust(mat2gray(H)),'XData',T,'YData',rho,'InitialMagnification','fit');
+  axis on, axis normal, hold on;
+  colormap(hot);
+
+  % find peaks
+  peaks = houghpeaks(H);
+
   % rotate image, using nearest neighbour TO-DO: can other interpolations be used?
   rotated_img = imrotate(img,90-degree);
-  %figure, imshow(rotated_img);
+  figure, imshow(rotated_img);
 
 return;
