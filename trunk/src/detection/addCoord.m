fb5e652b4856d02e58e41e87ec64653c4265eec7
esbenbugge <esbenbugge@1f401a1f-e745-0410-a33a-219f3c91b65c>
2008-03-13 20:43:02 +0000

    pdfs from SKAT. done with addCoord and getCoord and locate_plate

--- trunk/src/detection/addCoord.m
+++ trunk/src/detection/addCoord.m
@@ -1,33 +1,37 @@
-% Function to add some coordinates to a filename. The current filename must
+% Function to add coordinates to a filename. The current filename must
 % be on form [P][F][B]_XXXXXXX.jpg, where 'XXXXXXX' is the characters of
-% the plate. The user inputs two mouseclicks specifing the upper-left
-% corner and the lower-right corner of the licenseplate. The new filename
-% will be on the form [P][F][B]_xMin-xMax-yMin-yMax_XXXXXXX.jpg.
+% the plate. The new filename will be on the form
+% [P][F][B]_xMin-xMax-yMin-yMax_XXXXXXX.jpg.
 function [] = addCoord (file, xMin, xMax, yMin, yMax)
   
-  % make new filename
+  % find beginning of filename (position of [P][F][B])
   pathLength = length(file);
-  fileNameStart = pathLength - 12
+  fileNameStart = pathLength - 12;
   
-  % pad 0's
-  %coords = [xMin, xMax, yMin, yMax];
+  % pad 0's in front of every coordinate
+  coords = [xMin, xMax, yMin, yMax];
+  coordStr = '';
   
-  %for c = coords
-  %  if c < 10
-  %    coordinates = strcat(coordinates,'000',int2str(c),'-');
-  %  elseif c < 100
-  %    coordinates = strcat(coordinates,'00',int2str(c),'-');
-  %  elseif c < 1000
-  %    coordinates = strcat(coordinates,'0',int2str(c),'-');
-  %  else
-  %    coordinates = strcat(coordinates,int2str(c),'-');
-  %  end
-  %end
+  for c = 1:4
+    if coords(c) < 10
+      coordStr = strcat(coordStr,'000',int2str(coords(c)));
+    elseif coords(c) < 100
+      coordStr = strcat(coordStr,'00',int2str(coords(c)));
+    elseif coords(c) < 1000
+      coordStr = strcat(coordStr,'0',int2str(coords(c)));
+    else
+      coordStr = strcat(coordStr,int2str(coords(c)));
+    end
+    % if the coordinate is not the last, add a '-' afterwards
+    if c ~= 4
+      coordStr = strcat(coordStr,'-');
+    end
+  end
   
-  coordinates = strcat(int2str(xMin),'-',int2str(xMax),'-',int2str(yMin),'-',int2str(yMax));
-  newName = strcat(file(1:fileNameStart+1),coordinates,'_',file(fileNameStart+2:pathLength));
+  %coordinates = strcat(int2str(xMin),'-',int2str(xMax),'-',int2str(yMin),'-',int2str(yMax));
   
-  % rename file
+  % create new filename and rename the file
+  newName = strcat(file(1:fileNameStart+1),coordStr,'_',file(fileNameStart+2:pathLength));
   movefile(file,newName);
   
 return
--- trunk/src/detection/char_segment.m
+++ trunk/src/detection/char_segment.m
@@ -1,26 +1,23 @@
 % Function to pick out the chars of a cut-out and rotated licenseplate.
-% 
-function [chars] = char_segment (img) 
+function [chars] = char_segment (plateImg) 
 
   % transform image to binary and show the binary image, WHAT IS LEVEL 
   %level = graythresh(img);
   %level = 0.6;
   %bw_img = im2bw(img,level);
-  bw_img = im2bw(img);
+  bwImg = im2bw(plateImg);
   
   % display binary image
-  figure(100);
-  subplot(2,1,1);
-  imshow(bw_img);
+  figure(1), subplot(3,1,1), imshow(bwImg);
 
   % calculate no. of scanlines
-  h_scanlines = size(bw_img,1);
+  h_scanlines = size(bwImg,1);
   
   % find the vertical middle of the image
-  v_middle = floor(h_scanlines/2)
+  v_middle = floor(h_scanlines/2);
 
   % calculate the sums of all horizontal scanlines
-  h_scanlines_sums = sum(bw_img,2);
+  h_scanlines_sums = sum(bwImg,2);
 
   % find top- and bottom sums, NOT WORKING
   top_sums = h_scanlines_sums(1:v_middle,:);
@@ -31,22 +28,20 @@ function [chars] = char_segment (img)
 
   % fill out horizontal lines outside plate with 1's (white)
   for l = 1:top_line
-    bw_img(l,:) = 1;
+    bwImg(l,:) = 1;
   end
 
-  for l = bottom_line:size(bw_img,1)
-    bw_img(l,:) = 1;
+  for l = bottom_line:size(bwImg,1)
+    bwImg(l,:) = 1;
   end
 
   % display 'cut' image, everything above and below the plate should
   % now be white
-  figure(100);
-  subplot(2,1,2);
-  imshow(bw_img);
+  figure(1), subplot(3,1,2), imshow(bwImg);
 
   % calculate total no. of vertical scanlines and the pixel-sum of each line
-  v_scanlines = size(bw_img,2);
-  v_scanlines_sums = sum(bw_img,1);
+  v_scanlines = size(bwImg,2);
+  v_scanlines_sums = sum(bwImg,1);
 
   % there are 14 vertical cuts to be made, two for each of the seven chars
   cuts = zeros(14,1);
@@ -63,14 +58,14 @@ function [chars] = char_segment (img)
   % iterate through vertical scanlines
   while s < v_scanlines
     % if the vertical line is all white
-    if v_scanlines_sums(s) == size(bw_img,1)
+    if v_scanlines_sums(s) == size(bwImg,1)
       firstchar_met = true;
       % position: in front of a char
-      if to_char && v_scanlines_sums(s+1) < size(bw_img,1)
+      if to_char && v_scanlines_sums(s+1) < size(bwImg,1)
         cuts(cut) = s;
         cut = cut + 1;
       % position: at the end of a char
-      elseif v_scanlines_sums(s+1) == size(bw_img,1) && char_width > min_char_width
+      elseif v_scanlines_sums(s+1) == size(bwImg,1) && char_width > min_char_width
         cuts(cut) = s;
         cut = cut + 1;
         char_width = 0;
@@ -86,7 +81,10 @@ function [chars] = char_segment (img)
   % ONLY RETURNING ONE CHAR
   %c = 1;
   %for c = 1:7
-  chars = img(top_line:bottom_line,cuts(1):cuts(1+1));
+  chars = plateImg(top_line:bottom_line,cuts(1):cuts(1+1));
   %end
+  
+  % display chars
+  figure(1), subplot(3,1,3), imshow(chars);
   
 return;
