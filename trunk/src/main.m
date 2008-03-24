7983e1f7c5d28f1a9e9c6108cbdae76ca6762d5e
tobiasbp <tobiasbp@1f401a1f-e745-0410-a33a-219f3c91b65c>
2008-03-24 23:25:32 +0000

    Tilfoejet misc stat-print

--- trunk/src/main.m
+++ trunk/src/main.m
@@ -18,9 +18,16 @@ function [] = main(imagesFolder, numberOfImagesToProcess)
 addpath('detection');
 
 % noOfImages = 0;
+noOfPlatesFound = 0;
 percentageOfPlatesFound = 0;
 percentageOfPlatesRead = 0;
 percentageOfCharsRead = 0;
+
+% Number of times there weas no candidate
+noCandidate = 0;
+
+% echo time
+datestr(now)
 
 % Get filelist
 fileList = dir([imagesFolder '*.JPG']);
@@ -30,23 +37,53 @@ noOfImages = length(fileList);
 if noOfImages < 1 
   'No images found. Aborting.'
 else
-  ['Going to work on' noOfImages ' images.']
+  ['Going to work on ' int2str(noOfImages) ' images.']
 end
 
 for i = 1:noOfImages
 %for i = 1:1
-  % FIND PLATE
-  plateCoords = detect_lines([imagesFolder fileList(i).name])
-  %plateImage = detect2([imagesFolder fileList(i).name])
+
+  %%%%%%%%%%%%%%
+  % FIND PLATE %
+  %%%%%%%%%%%%%%
+
+  % Get plate coordinates from filename
+  % xMin, xMax, yMin, yMax
+  realPlateCoords = [str2num(fileList(i).name(1,3:6)), str2num(fileList(i).name(1,8:11)), ...
+                     str2num(fileList(i).name(1,13:16)), str2num(fileList(i).name(1,18:21))];
+ 
+  % Analyze image and get coordinates of plate
+  plateCoords = detect_lines([imagesFolder fileList(i).name]);
+  %plateCoords = detect2([imagesFolder fileList(i).name])
+  %plateCoords = detect4([imagesFolder fileList(i).name])
+  %plateImage = detect3([imagesFolder fileList(i).name])
   
-  % ROTATE
-  rotatedPlateImg = plate_rotate([imagesFolder fileList(i).name],plateCoords(1),plateCoords(2),plateCoords(3),plateCoords(4));
+  % Determine if plate is within found coordinates 
+  if (realPlateCoords(1) >= plateCoords(1) && realPlateCoords(2) <= plateCoords(2) && ...
+   realPlateCoords(3) >= plateCoords(3) && realPlateCoords(4) <= plateCoords(4))
+    noOfPlatesFound = noOfPlatesFound + 1;
+  else
+    % Echo name of image where plate was not found
+    fileList(i).name
+    % No candidate was found
+    if sum(plateCoords) == 0
+      noCandidate = noCandidate + 1;
+    end
+  end   
+
+
+
+  %%%%%%%%%%
+  % ROTATE %
+  %%%%%%%%%%
+  %rotatedPlateImg = plate_rotate([imagesFolder fileList(i).name],plateCoords(1),plateCoords(2),plateCoords(3),plateCoords(4));
   
   % SEGMENT CHARS
   %chars = char_segment(rotatedPlateImg);
   
   % Wait for user to press a key
-  pause();
+  % pause();
+  
   % SomeFunction([imagesFolder fileList(i).name]);
   
 
@@ -55,3 +92,13 @@ end
 
 
 % PRINT STATS
+
+percentageOfPlatesFound = noOfPlatesFound*(100/noOfImages)
+
+% What percentage of the candidates were correct
+correctnessOfCandidates = noOfPlatesFound*(100/(noOfImages-noCandidate))
+
+%noOfPlatesNotFound = noOfImages - noOfPlatesFound
+
+% echo time
+datestr(now)
