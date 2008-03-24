2edd4ee8fa3d278ae93b38ae22f8f98dc478213c
tobiasbp <tobiasbp@1f401a1f-e745-0410-a33a-219f3c91b65c>
2008-03-24 23:24:36 +0000

    Div. updates til detektion af plader. Nu 88,3% korrekt

---
+++ src/detection/detect4.m
@@ -0,0 +1,277 @@
+% Finds 51.5% of all plates fed
+% If function returns coordinates correctness is 88%
+
+function plateCoords = detect4(inputImage)
+
+
+showImages = false;
+
+
+% Downscale factor
+downscaleFactor = 4;
+
+
+
+
+
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Read image from file as gray %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+origImage = rgb2gray(imread(inputImage));
+
+
+%%%%%%%%%%%%%%%%%%%%
+% Downsample image %
+%%%%%%%%%%%%%%%%%%%%
+
+% Downsample image columns
+scaledImage = downsample(origImage,downscaleFactor);
+% Downsample image rows
+scaledImage = transpose(downsample(transpose(scaledImage),downscaleFactor));
+
+% Image width and height
+imHeight = size(scaledImage,1);
+imWidth = size(scaledImage,2);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Log image %
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+logImage = log10(double(scaledImage));
+logImage = uint8((256/(max(max(logImage)))) .* logImage);
+
+
+
+%bwImage = im2bw(logImage);
+
+
+%%%%%%%%%%%%%%%%%%%%
+% Contrast stretch %
+%%%%%%%%%%%%%%%%%%%%
+
+
+% Create histogram
+imHist = hist(double(logImage(:)),256);
+
+% Find highest location of highest value in histogram
+histPeakPos = find(imHist >= max(imHist));
+
+minCut = histPeakPos - 30;
+maxCut = histPeakPos + 10;
+
+if maxCut >= 256
+  maxCut = 256;
+end
+
+
+%histPeakPos/255
+
+%contImage = im2bw(logImage,histPeakPos/256);
+
+contImage = logImage;
+%contImage = scaledImage;
+
+
+
+for row  = 1:imHeight
+  for col = 1:imWidth
+   if (contImage(row,col) <= minCut)
+     contImage(row,col) = 0;
+   elseif (contImage(row,col) >= maxCut)
+     contImage(row,col) = 255;
+   else
+     %contImage(row,col) = (256-1) * ((contImage(row,col) - minCut)/(maxCut-minCut))
+   end
+  end 
+end
+
+%figure(200), imshow(im2bw(contImage));
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Erode image to separate plates from cars %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Erode to make components in plate connect
+%Shape to use for dilation
+line = strel('line',3,0);
+%line = strel('line',10,2);
+%line = strel('line',10,5);
+%line = strel('line',6,22);
+%ball = strel('ball',2,2);
+%shape = strel('square',2);
+%se = strel('disk',2,4);
+
+%contImage = imerode(contImage,shape);
+%contImage = imclose(contImage,shape);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Calculate connected components %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
+% conComp = matrix holding components
+% numConComp = Number of connected components
+[conComp,numConComp] = (bwlabel(contImage,4));
+
+
+% Delete some components
+
+% Components with width og height = the image
+
+
+
+
+
+% Lets find out which connected component in the image has the most
+% plate-like width/height ratio 504/120 is official size
+
+%optimalPlateRatio = 504/120; % 55%
+
+
+% Ratio from image
+%optimalPlateRatio = 182/37; % 56,7%
+optimalPlateRatio = 182/42; % 57,3%
+
+
+
+% Loop through connected components. The component with the
+% best width/height-aspect is our numberplate
+
+% smallest diff between best components ratio and optimal plate ratio
+bestRatioDiff = inf;
+
+% The number of the component with the closest matchin aspect ratio
+bestRatioComponent = 0;
+
+% If no component is chosen, well return this
+
+%minX = 0;
+%maxX = 0;
+%minY = 0;
+%maxY = 0;
+
+
+for i = 1:numConComp
+  
+  % Get Xs and Ys of current component
+  [Ys,Xs] = find(conComp == i);
+
+  % Calculate with height of component
+  compHeight = max(Ys)-min(Ys);
+  compWidth = max(Xs)-min(Xs);
+  compLength = length(Xs);
+
+  % Calculate width/height-ratio of current component
+  compRatio = compWidth/compHeight;
+
+  % Very big components are bad
+  %if compHeight < imHeight/4 && compWidth < imWidth/4
+
+  % The sizes of the plates are smaller than this (numbers are unscaled sizes)
+  if compHeight < 80/downscaleFactor && compHeight > 30/downscaleFactor && ...
+      compWidth < 250/downscaleFactor && compWidth > 130/downscaleFactor && ...
+      compLength >= 250 % Component should consist of a min number of fixels
+  
+    % compLength
+    % Calculate difference between current components ratio
+    % and ratio of plate
+    ratioDiff = abs(optimalPlateRatio-compRatio);
+
+    if ratioDiff < bestRatioDiff && ((max(Xs)-min(Xs))*downscaleFactor) > 20
+      bestRatioDiff = ratioDiff;
+      bestRatioComponent = i;
+    end
+  end
+end
+
+
+% Get coords for best matching component
+[Ys,Xs] = find(conComp == bestRatioComponent);
+
+% Calculate coords
+minX = downscaleFactor*min(Xs);
+maxX = downscaleFactor*max(Xs);
+minY = downscaleFactor*min(Ys);
+maxY = downscaleFactor*max(Ys);
+
+
+
+
+
+
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%
+% Plots            %
+%%%%%%%%%%%%%%%%%%%%
+
+if showImages
+  figure(200), imshow(label2rgb(conComp));
+
+
+  figure(100);
+
+
+  % Image
+  subplot(2,3,1);
+  imshow(scaledImage);
+%imshow(image);
+
+
+  % Gradients
+  subplot(2,3,2);
+  imshow(logImage);
+
+  subplot(2,3,3);
+  %figure, imshow(contImage);
+  imshow(contImage);
+
+  % Summed gradients
+  subplot(2,3,4);
+  hist(double(logImage(:)),256)
+
+  %imshow(summedGradsXNorm);
+
+
+  % Summed grads as binary image
+  subplot(2,3,5);
+  imshow(conComp);
+  %hist(double(scaledImage(:)),256)
+  %imshow(summedGradsXThresh);
+
+
+  subplot(2,3,6);
+  if bestRatioDiff < inf
+    imshow(origImage(minY:maxY,minX:maxX),[]);
+  end
+
+  %imshow(FX);
+end
+
+if bestRatioDiff < inf
+  plateCoords = [(minX-10) (maxX+10) (minY-10) (maxY+10)];
+else
+  % No component was selected but we need to return something
+  plateCoords = [ 0 0 0 0 ];
+end
+
+
+
+end 
--- src/detection/detect_lines.m
+++ src/detection/detect_lines.m
@@ -1,17 +1,48 @@
 function plateCords = detect_lines(inputImage)
 
 % Downscale factor
-downscaleFactor = 4;
+downscaleFactor = 2;
+
+
+
+% get candidate coordinate from detect4
+d4C = detect4(inputImage) ./ downscaleFactor;
 
 % Read image from file
 origImage = imread(inputImage);
 
+
+
+
 % Convert image to grayscale
 image = rgb2gray(origImage);
 
+
+
+
+% Brighten image
+%brighten(1);
+
+
 % Log image
-%image = log10(double(image))/log10(512);
-%figure, imshow(log(double(image)),[]);
+image = log10(double(image));
+image = uint8((256/(max(max(image)))) .* image);
+
+
+%image = image - 100;
+
+%image = double(max(max(image))) ./ double(image);
+
+%image = image .* 256;
+
+
+%max(max(image))
+%min(min(image))
+
+%figure, imshow(image,[]);
+
+
+
 
 
 % Downsample image columns
@@ -19,16 +50,24 @@ image = downsample(image,downscaleFactor);
 % Downsample image rows
 image = transpose(downsample(transpose(image),downscaleFactor));
 
+
 % Image width and height
 imHeight = size(image,1);
 imWidth = size(image,2);
 
 
 
+
+
+
+
+
 figure(100);
 
 
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Create image of strongest gradients         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Experiment with gradients
 [FX,FY] = gradient(double(image));
 %figure(10);
@@ -43,7 +82,7 @@ scanner = zeros(imHeight, imWidth);
 for x = 1:imWidth
   for y = 1:imHeight
 %    if abs(FX(y,x)) > (maxGradientX * 0.5)
-    if abs(FX(y,x)) > (maxGradientX * 0.4)
+    if abs(FX(y,x)) > (maxGradientX * 0.2)
       scanner(y,x) = 1;
     end  
   end
@@ -93,15 +132,21 @@ imshow(scanner);
 
 %Shape to use for dilation
 %line = strel('line',6,0);
-line = strel('line',10,2);
+
+%shape = strel('line',10,2); % Den gamle
+
+
 %line = strel('line',10,5);
 %line = strel('line',6,22);
 %ball = strel('ball',1,1);
-%square = strel('square',3);
+%shape = strel('square',3);
 %se = strel('disk',2,4);
 
+shape= strel('rectangle', [3,10]); % For scale = 2
+%shape= strel('rectangle', [2,5]); % for scale = 4
+
 %scanner = imdilate(scanner,ball);
-scanner = imdilate(scanner,line);
+scanner = imdilate(scanner,shape);
 %scanner = imerode(scanner,se);
 
 % Show dilated image
@@ -149,16 +194,18 @@ imshow(scanner);
 % REMOVING LEAST WHITE LINES %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Not effectice % REMOVED %
+
 % Find value of most white (largest sum) horizontal line
-rowWhiteValues = sum(transpose(scanner));
-maxRowWhiteValue = max(rowWhiteValues);
+%rowWhiteValues = sum(transpose(scanner));
+%maxRowWhiteValue = max(rowWhiteValues);
 
 % Set all values for rows with low white-values to 0
-for row  = 1:imHeight
-  if (rowWhiteValues(row) < (0.6 * maxRowWhiteValue))
-    %scanner(row,:) = 0;
-  end 
-end
+%for row  = 1:imHeight
+%  if (rowWhiteValues(row) < (0.2 * maxRowWhiteValue))
+%    scanner(row,:) = 0;
+%  end 
+%end
 
 % Show image with weak lines removed
 %figure(15) , imshow(scanner);
@@ -170,15 +217,24 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %Shape to use for dilation
-line = strel('line',15,22);
+%shape = strel('line',15,22);
+
 %line = strel('line',10,2);
 %line = strel('line',10,5);
 %line = strel('line',6,22);
-%ball = strel('ball',1,1);
-%square = strel('square',5);
+%shape = strel('ball',3,3);
+%shape = strel('square',4);
+%shape = strel('square',4);
 %se = strel('disk',2,4);
-scanner = imdilate(scanner,line);
 
+shape = strel('rectangle', [10,6]); % For scale = 2
+%shape = strel('rectangle', [5,3]); % For scale = 4
+
+scanner = imdilate(scanner,shape);
+
+% Instead of dilating here, I'll return parts of the
+% image below and over the component when calculation coordinates
+% Hmm.. But then I'd need to take care of coords outside the image
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Use biggest component to get plate position %
@@ -186,7 +242,7 @@ scanner = imdilate(scanner,line);
 
 % If more than one candidate. The one closest to the center should win.
 % Or beeing close to the edge is not good
-% Another posibility is choosing the are with the most plate like ratio
+% Another posibility is choosing the area with the most plate like ratio
 
 
 % Create components
@@ -203,24 +259,43 @@ bottomLeft = [1,1];
 bottomRight = [1,1];    
 
 
-for i = 1:numConComp
-  % Get Xs and Ys of current component
-  [Ys,Xs] = find(conComp == i);
-  % Get size of component
-  compSize = length(Xs);
-  if (compSize > biggestComponentSize)
-    %biggestComponent = i;
-    biggestComponentSize = compSize;
-%    topLeft = [min(Ys),min(Xs)];
-%    topRight = [min(Ys),max(Xs)];
-%    bottomLeft = [max(Ys),min(Xs)];
-%    bottomRight = [max(Ys),max(Xs)];    
-    topLeft = downscaleFactor*[min(Ys),min(Xs)];
-    topRight = downscaleFactor*[min(Ys),max(Xs)];
-    bottomLeft = downscaleFactor*[max(Ys),min(Xs)];
-    bottomRight = downscaleFactor*[max(Ys),max(Xs)];    
-  end
+% Check if one of our candidates includes the
+% center of detect4's canditate
+if sum(d4C) > 0 
+  d4Thinks = conComp(round(sum(d4C(3:4))/2),round(sum(d4C(1:2))/2)) 
+else
+  d4Thinks = 0
 end
+
+
+if d4Thinks > 0
+  % d4 has a candidate which is also one of our
+  % candidates. Let's return d4s coordinates
+  % as its candidates are well-sized
+  topLeft = downscaleFactor*[d4C(3),d4C(1)];
+  topRight = downscaleFactor*[d4C(3),d4C(2)];
+  bottomLeft = downscaleFactor*[d4C(4),d4C(1)];
+  bottomRight = downscaleFactor*[d4C(4),d4C(2)];    
+
+else
+  for i = 1:numConComp
+    % Get Xs and Ys of current component
+    [Ys,Xs] = find(conComp == i);
+    % Get size of component
+    compSize = length(Xs);
+
+    if (compSize > biggestComponentSize && min(Ys) > 5 && ...
+        max(Ys) < imHeight-5 && min(Xs) > 5 && ... 
+        max(Xs) < imWidth-5 )
+      %biggestComponent = i;
+      biggestComponentSize = compSize;
+      topLeft = downscaleFactor*[min(Ys),min(Xs)];
+      topRight = downscaleFactor*[min(Ys),max(Xs)];
+      bottomLeft = downscaleFactor*[max(Ys),min(Xs)];
+      bottomRight = downscaleFactor*[max(Ys),max(Xs)];    
+    end % component
+  end % component loop
+end % d4Thinks
 
 
 % If detected area does not match ratio of plate. Consider making it wider.
@@ -293,4 +368,6 @@ image = scanner;
 
 
 plateCords = [topLeft(2), topRight(2), topLeft(1), bottomLeft(1)];
+
+
 return;
