72b64246bdce78ff5666443795fb16c0742abcb6
esbenbugge <esbenbugge@1f401a1f-e745-0410-a33a-219f3c91b65c>
2008-04-17 11:17:14 +0000

    renamed many pdfs, written rapport and billedbehandling, moved litteratur into rapport, added mser pdf, added first version of function that can segment cahrs using peak to valley

--- trunk/src/detection/plate_rotate_radon.m
+++ trunk/src/detection/plate_rotate_radon.m
@@ -8,14 +8,22 @@ function [rotatedPlateImg] = plate_rotate_radon (imgFile, xMin, xMax, yMin, yMax
   
   % display input img
   if figuresOn
-    figure(1), subplot(2,2,1), imshow(grayImg), title('greyed input image');
+    figure(11), subplot(2,2,1), imshow(grayImg), title('greyed input image');
   end
   
   % pick out plate image and show it
   plateImg = grayImg(yMin:yMax, xMin:xMax);
   if figuresOn
-    figure(1), subplot(2,2,2), imshow(plateImg), title('plate image');
+    figure(11), subplot(2,2,2), imshow(plateImg), title('plate image');
   end
+  
+  
+  %plateImg2 = uint8((double(plateImg)/180)*256);
+  %imgContrastEnh = im2bw(plateImg,graythresh(plateImg));
+  
+  %if figuresOn
+  %  figure(11), subplot(2,3,3), imshow(plateImg2), title('brigtnessEnh bw image');
+  %end
   
   % compute binary edge image, TO-DO: determine kind of edge-function
   bwPlateImg = edge(plateImg,'sobel','horizontal');
@@ -24,8 +32,13 @@ function [rotatedPlateImg] = plate_rotate_radon (imgFile, xMin, xMax, yMin, yMax
   %bwPlateImg = edge(plateImg,'log');
   %bwPlateImg = edge(plateImg,'canny');
   if figuresOn
-    figure(1), subplot(2,2,3), imshow(bwPlateImg), title('edge image');
+    figure(11), subplot(2,2,3), imshow(bwPlateImg), title('edge image');
   end
+  
+  %bwPlateImg = edge(plateImg2,'sobel','horizontal');
+  %if figuresOn
+  %  figure(11), subplot(2,3,5), imshow(bwPlateImg), title('edge image: enh');
+  %end
 
   % compute radon transform of edge image, TO-DO: Only in 80:100
   [radonMatrix,xp] = radon(bwPlateImg,80:100);
@@ -79,7 +92,7 @@ function [rotatedPlateImg] = plate_rotate_radon (imgFile, xMin, xMax, yMin, yMax
   
   % display rotated image
   if figuresOn
-    figure(1), subplot(2,2,4), imshow(rotatedPlateImg), title('rotated plate image');
+    figure(11), subplot(2,2,4), imshow(rotatedPlateImg), title('rotated plate image');
   end
 
 end
