20fadba1b8ebadee55c853c4427cad8fb9071702
tobiasbp <tobiasbp@1f401a1f-e745-0410-a33a-219f3c91b65c>
2008-04-18 19:22:36 +0000

    Added functions for sig and plateness

---
+++ trunk/src/detection/GetPlateness.m
@@ -0,0 +1,39 @@
+% Takes a signature, returns plateness integer
+function plateness = GetSignature(sig)
+
+
+% Calculate plateness (number of times plot crosses avg. value in sig) 
+
+% Average value for sig
+% Plates often start and end low, We can move the avg up to try to compensate
+sigAvg = 1.1 * (sum(sig)/length(sig));
+
+% How many times have we crossed the avg. sig value
+timesCrossed = 0;
+
+% Do we start over or under avg. ?
+if sig(1) > sigAvg
+ over = true;
+else
+ over = false;
+end
+
+for x = 2:length(sig)
+  % Go down
+  if over == true && sig(x) <= sigAvg
+    timesCrossed = timesCrossed + 1;
+    over = false;
+  end
+  % Go up
+  if over == false && sig(x) > sigAvg
+    timesCrossed = timesCrossed + 1;
+    over = true;
+  end
+end
+
+
+% Plateness to return
+plateness = timesCrossed
+
+
+end
