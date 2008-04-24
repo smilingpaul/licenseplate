7e4842f44eb96fa4cb3a13453d1c413bf981caab
esbenbugge <esbenbugge@1f401a1f-e745-0410-a33a-219f3c91b65c>
2008-04-24 15:34:18 +0000

    made new method in histo_detect, made edits to rapport etc following review, made correction in plate_rotate_radon

--- trunk/src/detection/make_freq_table.m
+++ trunk/src/detection/make_freq_table.m
@@ -2,68 +2,92 @@
 %%%%%%%%%%%%% all images in a folder
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function normFreqTable = make_freq_table (imgFolder)
+function [horizontalTable, verticalTable] = make_freq_table (imgFolder)
 
   fileList = dir([imgFolder '*.JPG']);
   noOfImages = length(fileList);
 
   ['Making frequency table for ' int2str(noOfImages) ' images.']
+  
+  % create table to hold frequencies: OLD METHOD
+  %freqTable = zeros(256,256,256);
 
-  % create table to hold frequencies
-  freqTable = zeros(255,255,255);
-
-  % create table to hold normalized frequencies
-  normFreqTable = zeros(size(freqTable));
-
-  for i =1:noOfImages
+  % create table to hold normalized frequencies: OLD METHOD
+  %normFreqTable = zeros(size(freqTable));
+  
+  %% NEW METHOD: 16 x 16 x ... tables
+  horizontalTable = zeros(16,16,16,16,16,16);
+  verticalTable = zeros(16,16,16,16,16,16);
+  
+  
+  for i = 1:noOfImages
     % get coordinates of plate and read image
-    %strcat(imgFolder, fileList(i).name)
-    [xMin, xMax, yMin, yMax] = getCoord(fileList(i));
-
-    %imgFile.name(1,3:6)
-    %realPlateCoords = [str2num(imgFile.name(1,3:6)), str2num(imgFile.name(1,8:11)), ...
-    %                            str2num(imgFile.name(1,13:16)), str2num(imgFile.name(1,18:21))];
-
+    plateCoords = [str2num(fileList(i).name(1,3:6)), ...
+      str2num(fileList(i).name(1,8:11)), ...
+      str2num(fileList(i).name(1,13:16)), ...
+      str2num(fileList(i).name(1,18:21))];
+    
     img = imread(strcat(imgFolder, fileList(i).name));
-    subImg = img(yMin:yMax,xMin:xMax,:);
-    %subImg = img(realPlateCoords(3):realPlateCoords(4),realPlateCoords(1):realPlateCoords(2),:);
-    figure(44), imshow(subImg), title('plate image');
-
+    plateImg = img(plateCoords(3):plateCoords(4),...
+                plateCoords(1):plateCoords(2),:);
+              
+    figure(44), imshow(plateImg), title('plate image');
+    
     % get dimensions of image
-    imgHeight = size(subImg,1);
-    imgWidth = size(subImg,2);
+    imgHeight = size(plateImg,1);
+    imgWidth = size(plateImg,2);
 
-    % iterate through image
-    for y = 1:imgHeight
-      for x = 1:imgWidth
-        r = subImg(y,x,1) + 1;
-        g = subImg(y,x,2) + 1;
-        b = subImg(y,x,3) + 1;
-        freqTable(r,g,b) = freqTable(r,g,b) + 1;
+    % iterate through image and register frequencies: OLD METHOD
+    %for y = 1:imgHeight
+    %  for x = 1:imgWidth
+    %    r = plateImg(y,x,1) + 1;
+    %    g = plateImg(y,x,2) + 1;
+    %    b = plateImg(y,x,3) + 1;
+    %    freqTable(r,g,b) = freqTable(r,g,b) + 1;
+    %  end
+    %end
+    
+    % iterate through image and register frequencies: NEW METHOD
+    for y = 1:imgHeight-1
+      for x = 1:imgWidth-1
+        
+        r = floor(double(plateImg(y,x,1))/16)+1;
+        g = floor(double(plateImg(y,x,2))/16)+1;
+        b = floor(double(plateImg(y,x,3))/16)+1;
+        
+        rRight = floor(double(plateImg(y,x+1,1))/16)+1;
+        gRight = floor(double(plateImg(y,x+1,2))/16)+1;
+        bRight = floor(double(plateImg(y,x+1,3))/16)+1;
+        
+        rBelow = floor(double(plateImg(y+1,x,1))/16)+1;
+        gBelow = floor(double(plateImg(y+1,x,2))/16)+1;
+        bBelow = floor(double(plateImg(y+1,x,3))/16)+1;
+        
+        % sum up frequencies
+        horizontalTable(r,g,b,rRight,gRight,bRight) = ...
+          horizontalTable(r,g,b,rRight,gRight,bRight) + 1;
+        verticalTable(r,g,b,rBelow,gBelow,bBelow) = ...
+          verticalTable(r,g,b,rBelow,gBelow,bBelow) + 1;
+        
       end
     end
+    
 
     %%%%%%%%%%%%%%%%%% normalizing %%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    % find frequency of most occuring color
-    maxFreq = max(max(max(freqTable)));
-    %1 / maxFreq
-    %normFreqTable(5,4,2) = freqTable(5,4,2)/maxFreq;
-    %normFreqTable(5,4,2)
+    % find frequency of most occuring color: OLD METHOD
+    %maxFreq = max(max(max(freqTable)));
 
-    % normalize using maxFreq
-    %for r = 1:size(freqTable,1)
-    %  for g = 1:size(freqTable,2)
-    %    for b = 1:size(freqTable,3)
-    %      normFreqTable(r,g,b) = freqTable(r,g,b)/maxFreq;
-    %    end
-    %  end
-    %end
-    normFreqTable = freqTable/maxFreq;
-
-    % wait for user to press a key
-    %pause();
+    % normalize using maxFreq: OLD METHOD
+    %normFreqTable = freqTable/maxFreq;
+    
+    % normalize: NEW METHOD
+    maxFreqHori = max(max(max(max(max(max(horizontalTable))))));
+    maxFreqVert = max(max(max(max(max(max(verticalTable))))));
+    
+    horizontalTable = horizontalTable/maxFreqHori;
+    verticalTable = verticalTable/maxFreqVert;
 
   end
 
--- trunk/src/segmentation/plate_rotate_radon.m
+++ trunk/src/segmentation/plate_rotate_radon.m
@@ -1,102 +1,73 @@
-% Given an image of a licenseplate, rotate this image so the plate is
-% placed horizontal in the image. Input image  must be two-dimensional.
+% Given an image of a car with a licenseplate, rotate this image so the plate is
+% placed horizontal in the image. The function only rotates the image if
+% the rotation degree found during the analysis is >= minRotation. The
+% maximum rotation is set to 10 as it shouldn't be necsseasry to rotate the
+% images more than this (lav dette til en variable også??).
 %
 % Input parameters:
-% - imgFile: file containing the image with a plate
-% - xMin, Xmax, yMin, yMax: the coordinates of the plate in the image.
+% - imgFile: file containing the image with a plate.
+% - plateCoords: the coordinates of the plate in the image.
 % - figuresOn: true/false whether figures should be printed.
 %
 % Output parameters:
-% - rotatedPlateImg: the image of the plate (and only the plate), rotated
-function [rotatedPlateImg] = plate_rotate_radon (imgFile, xMin, xMax, yMin, yMax, figuresOn)
+% - rotatedImg: the image, rotated so the plate is horizontally placed.
+% - plateCoords: the coordinates of the plate in the rotated image.
+function [rotatedImg, plateCoords] = plate_rotate_radon (imgFile, plateCoords, figuresOn)
   
-  % read image
+  % specify the minimum rotation degree. Image will not be rotated if the
+  % analysis indicates a rotation lower than this.
+  minRotation = 1;
+
+  % read image and make it grayscale
   img = imread(imgFile);
   grayImg = rgb2gray(img);
   
-  % display input img
-  if figuresOn
-    figure(11), subplot(2,2,1), imshow(grayImg), title('greyed input image');
-  end
-  
   % pick out plate image and show it
-  plateImg = grayImg(yMin:yMax, xMin:xMax);
+  plateImg = grayImg(plateCoords(3):plateCoords(4), plateCoords(1):plateCoords(2));
   if figuresOn
-    figure(11), subplot(2,2,2), imshow(plateImg), title('plate image');
+    figure(11), subplot(3,1,1), imshow(plateImg), title('input plate image');
   end
   
+  % enhance contrast etc. TO-DO??
   
-  %plateImg2 = uint8((double(plateImg)/180)*256);
-  %imgContrastEnh = im2bw(plateImg,graythresh(plateImg));
-  
-  %if figuresOn
-  %  figure(11), subplot(2,3,3), imshow(plateImg2), title('brigtnessEnh bw image');
-  %end
-  
-  % compute binary edge image, TO-DO: determine kind of edge-function
+  % compute binary edge image from grayImg
   bwPlateImg = edge(plateImg,'sobel','horizontal');
   %bwPlateImg = edge(plateImg,'prewitt','horizontal');
   %bwPlateImg = edge(plateImg,'roberts'); VERY BAD
   %bwPlateImg = edge(plateImg,'log');
   %bwPlateImg = edge(plateImg,'canny');
   if figuresOn
-    figure(11), subplot(2,2,3), imshow(bwPlateImg), title('edge image');
+    figure(11), subplot(3,1,2), imshow(bwPlateImg), title('edge image, normal');
   end
-  
-  %bwPlateImg = edge(plateImg2,'sobel','horizontal');
-  %if figuresOn
-  %  figure(11), subplot(2,3,5), imshow(bwPlateImg), title('edge image: enh');
-  %end
 
   % compute radon transform of edge image, TO-DO: Only in 80:100
   [radonMatrix,xp] = radon(bwPlateImg,80:100);
   
   % display radon matrix
   %theta = 0:179;
-  %figure(200);
-  %imagesc(80:100, xp, radonMatrix); colormap(hot);
+  figure(111), imagesc(80:100, xp, radonMatrix); colormap(hot);
   %xlabel('\theta'); ylabel('x\prime');
-  %title('Radon transformation_{\theta} {x\prime}');
-  %colorbar
+  title('Radon transformation_{\theta} {x\prime}');
+  colorbar
 
   % find degree of which the largest registration in Radon transformation
   % matrix was found
   [x,degree] = max(max(abs(radonMatrix)));
-  degree = degree + 80; % plus 80 because of the 80:100 radonmatrix
-  
+  rotateDeg = 90 - (degree + 80 - 1) % plus 80 because of the 80:100 radonmatrix  
 
-  % convert the degree of rotation
-  rotateDeg = 90 - degree
+  % convert the degree of rotation to horizontal plane
+  %rotateDeg = 90 - degree + 1 % plus 1: correction, WHY IS IT NEEDED??
 
-  % only rotate if rotateDeg is between 3 and 10
-  %if abs(rotateDeg) > 10 || abs(rotateDeg) < 3
-  if abs(rotateDeg) < 3 % no need to check for > 10 because of 80:100
-    rotateDeg = 0;
-  end
-
-  % TEST OF HOUGH: TO-DO
-  %[H,T,rho] = hough(bw);
-  %display hough matrix
-  %figure, subplot(2,1,2);
-  %imshow(imadjust(mat2gray(H)),'XData',T,'YData',rho,'InitialMagnification','fit');
-  %axis on, axis normal, hold on;
-  %colormap(hot);
-  % find peaks
-  %peaks = houghpeaks(H)
-
-  % rotate image, using 'crop' to specify size of rotated image
-  %rotationMade = false;
-  
-  if rotateDeg ~= 0
+  % only rotate if rotateDeg is between minRotation and 10
+  if abs(rotateDeg) >= minRotation % no need to check for <= 10 because of 80:100
     rotatedImg = imrotate(img,rotateDeg,'bilinear','crop');
-    rotatedPlateImg = rotatedImg(yMin:yMax, xMin:xMax, :);
   else
-    rotatedPlateImg = img(yMin:yMax, xMin:xMax, :);
+    rotatedImg = img;
   end
   
   % display rotated image
   if figuresOn
-    figure(11), subplot(2,2,4), imshow(rotatedPlateImg), title('rotated plate image');
+    figure(11), subplot(3,1,3), imshow(rotatedImg(plateCoords(3):plateCoords(4), plateCoords(1):plateCoords(2), :)), title('rotated plate image');
   end
 
 end
